b2dfec36bc2e4bce2bd0515c70ef447346735ec4
Akash Singh <mr.singhaksh@gmail.com>
2024-03-06 20:55:31 +0530

    chore:update server url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://chat-room-bhvk.onrender.com/",
+        target: "https://chat-room-bhvk.onrender.com",
         secure: false,
       },
     },
