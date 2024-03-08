fe969f0e4586864c4f874533adc1e55ffd5fbe25
Akash Singh <mr.singhaksh@gmail.com>
2024-03-08 13:24:59 +0530

    chore:update axios instance

--- src/Config/axiosInstance.js
+++ src/Config/axiosInstance.js
@@ -3,16 +3,16 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://chit-chat-room-jfok.onrender.com",
 });
-axiosInstance.interceptors.request.use(
-  config => {
-    if (config.url && config.url.startsWith("/api/")) {
-      config.url = config.url.replace("/api/", "/");
-    }
-    return config;
-  },
-  error => {
-    return Promise.reject(error);
-  }
-);
+// axiosInstance.interceptors.request.use(
+//   config => {
+//     if (config.url && config.url.startsWith("/api/")) {
+//       config.url = config.url.replace("/api/", "/");
+//     }
+//     return config;
+//   },
+//   error => {
+//     return Promise.reject(error);
+//   }
+// );
 
 export default axiosInstance;
